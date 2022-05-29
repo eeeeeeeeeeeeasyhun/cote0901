294f84b20123bf63a0b13e7f2b1ceee4fb6efce4
eeeeeeeeeeeeasyhun <jihun5240@naver.com>
2022-05-29 23:50:14 +0900

    2132

--- cote0901/백준 15649 N과 M.cpp
+++ cote0901/백준 15649 N과 M.cpp
@@ -18,7 +18,7 @@ void dfs(int idx, int len)
 		cout << "\n";
 
 	}
-	else 
+	else  
 	{
 		for (int i = 1; i <= N; i++)
 		{
@@ -32,7 +32,7 @@ void dfs(int idx, int len)
 		}
 	}
 
-}
+}    
 
 int main()
 {
