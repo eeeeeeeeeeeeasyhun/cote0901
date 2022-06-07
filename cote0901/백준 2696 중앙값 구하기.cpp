95b86aa2a2783915648d4a092e2560d091ea31a6
eeeeeeeeeeeeasyhun <jihun5240@naver.com>
2022-06-07 23:01:03 +0900

    123

---
+++ cote0901/백준 2696 중앙값 구하기.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int main()
+{
+
+	int iTestCase;
+	cin >> iTestCase;
+	while (iTestCase--)
+	{  
+		int M;
+		vector<int> iArr;
+		vector<int> ans;
+		vector<int> SortV;
+	
+		for (int i = 0; i < M; i++)
+		{
+			int M;
+			
+			
+		}
+
+
+
+
+	}
+
+
+	
+
+
+}
