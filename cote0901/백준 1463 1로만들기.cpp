a6c0c4da93c489c55554c655e09bf917ab2b3458
eeeeeeeeeeeeasyhun <jihun5240@naver.com>
2022-06-08 16:36:12 +0900

    2022 06 08 -2

---
+++ cote0901/백준 1463 1로만들기.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int dp[1000001];
+
+int main()
+{
+
+	dp[1] = 0;
+	int N;
+	cin >> N;
+
+
+	for (int i = 2; i <= N; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], (dp[i / 2] + 1));
+		if (i % 3 == 0)
+			dp[i] = min(dp[i],(dp[i / 3] + 1));
+
+
+	}
+
+	cout << dp[N];
+}
