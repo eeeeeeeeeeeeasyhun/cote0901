e5debf54e24ddd50bfdb5ab0abf9c153e4c81607
eeeeeeeeeeeeasyhun <jihun5240@naver.com>
2022-06-09 23:53:26 +0900

    2022 06 09

---
+++ cote0901/백준 9095 1,2,3 더하기.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int dp[300];
+
+int main()
+{
+	int TestCase;
+	cin >> TestCase;
+
+	while (TestCase--)
+	{
+		int N;
+		cin >> N;
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;           
+
+		for (int i = 4; i <= N; i++)
+		{
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		cout << dp[N] << '\n';
+	}
+	
+
+
+
+
+}
