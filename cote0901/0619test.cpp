fa50cf38c116ad66235746bb203fa233d5f49f82
eeeeeeeeeeeeasyhun <jihun5240@naver.com>
2022-06-30 20:39:11 +0900

    0220 6 30

---
+++ cote0901/0619test.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+
+int main()
+ {
+	queue<tuple<int, int, int>> q;
+	q.front().push(1,1,1);
+
+		
+}  
