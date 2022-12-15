d01196df38f757af7ff2a9dabc1bff78b631fbc7
leejihun <jihun5240@naver.com>
2022-12-15 19:08:14 +0900

    [level 2] Title: 피보나치 수, Time: 0.49 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//int fib(int n)
+//{
+  //  if(n==0)
+   //     return 0;
+    //else if(n== 1)
+     //   return 1;
+    
+   // else
+     //   return (fib(n-1) + fib(n-2))  %1234567;
+    
+//}
+int solution(int n)
+{
+    int answer =0;
+    int fib[100001];
+    fib[0] =0;
+    fib[1] = 1;
+    for(int i=2; i<=n; i++)
+    {
+    fib[i] = (fib[i-1] + fib[i-2]) % 1234567;
+    }
+    
+     answer = fib[n];
+      return answer;
+}
