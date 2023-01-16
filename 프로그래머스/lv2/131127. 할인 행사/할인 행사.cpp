66a0d0af350f202a5f2597aed9473f53b4c20e85
leejihun <jihun5240@naver.com>
2023-01-17 08:22:18 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.cpp
@@ -1,21 +1,28 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <map>
 using namespace std;
 
-int solution(vector<string> want, vector<int> number, vector<string> discount) {
+int solution(vector<string> want, vector<int> number, vector<string> discount)
+{
     int answer = 0;
 
-    unordered_map<string, int> wantMap;
-    for(int i = 0; i < number.size(); i++)
-        wantMap.insert({want[i], number[i]});
+    map<string, int> mWant;
     
-    for(int i = 0; i <= discount.size() - 10; i++){
-        unordered_map<string, int> martDiscount;
-        for(int j = i; j < i + 10; j++){
-            martDiscount[discount[j]] += 1;
+    for(int i=0; i<number.size(); i++)
+    {
+        mWant.insert({want[i], number[i]});
+    }   
+    
+    for (int i = 0; i < discount.size() - 9; i++) 
+    {
+        map<string, int> mDiscount;
+        
+        for (int j = 0; j < 10; j++) 
+        {
+            mDiscount[discount[j + i]]++;
         }
-        if(martDiscount == wantMap)
+        if(mDiscount == mWant)
             answer++;
     }
 
